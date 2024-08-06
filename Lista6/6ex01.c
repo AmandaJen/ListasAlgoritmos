55ad28ede0ba4c43d6f6dd9879e2b92fdfa46e08
Ajen1237 <amandajen1237@gmail.com>
2024-08-06 12:21:20 -0300

    Exercicios

---
+++ Lista6/6ex01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int a[5][3], b[5][3], c[5][3], x, y;
+    for (x=0;x<5;x++){
+        for (y=0;y<3;y++){
+        scanf ("%i",&a[x][y]);
+        }
+    }
+    for (x=0;x<5;x++){
+        for (y=0;y<3;y++){
+        scanf ("%i",&b[x][y]);
+        }
+    }
+    for (x=0;x<5;x++){
+        for (y=0;y<3;y++){
+        c[x][y]=a[x][y]+b[x][y];
+        }
+    }
+    for (x=0;x<5;x++){
+        for (y=0;y<3;y++){
+        printf ("%i ", c[x][y]);
+        }
+    printf ("\n");
+    }
+    return 0;
+}
